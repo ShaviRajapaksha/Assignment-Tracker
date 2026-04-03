913d5c6eb123fdbae3550994f065c377220cef3f
IT20272654 <shavindurajapaksha@gmail.com>
2026-04-03 20:39:02 +0530

    feat: implement CORS support and set up Swagger documentation for API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Assignment Tracker API')
+    .setDescription('API documentation for the Assignment Tracker application')
+    .setVersion('1.0')
+    .addTag('assignments')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
