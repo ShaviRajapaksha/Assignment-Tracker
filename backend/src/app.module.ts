8bd20d950fb0f6adcb96d800b8c8dbbf1463e3fb
IT20272654 <shavindurajapaksha@gmail.com>
2026-04-04 21:19:14 +0530

    feat: add global configuration setup in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaModule } from './prisma/prisma.module';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
