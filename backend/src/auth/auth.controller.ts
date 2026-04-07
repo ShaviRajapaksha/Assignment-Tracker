583352e1dfb1a840478fb871ad8518cd47f9501b
IT20272654 <shavindurajapaksha@gmail.com>
2026-04-07 22:15:57 +0530

    feat: implement authentication endpoints for user registration and login

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { LoginDto, RegisterDto } from './dto/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    @ApiOperation({ summary: 'Register a new user' })
+    @ApiResponse({ status: 201, description: 'User successfully registered' })
+    register(@Body() registerDto: RegisterDto) {
+        return this.authService.register(registerDto);
+    }
+
+    @Post('login')
+    @ApiOperation({ summary: 'Login a user' })
+    @ApiResponse({ status: 200, description: 'User successfully logged in' })
+    login(@Body() loginDto: LoginDto) {
+        return this.authService.login(loginDto);
+    }
+}
